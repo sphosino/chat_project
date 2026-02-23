c42229d4d4672e1b586f9629bc20f861b1d49893
けんじ ひこまる <sphosino@gmail.com>
2026-02-24 08:15:22 +0900

    サインアップ画面修正

--- accounts/forms.py
+++ accounts/forms.py
@@ -8,10 +8,13 @@ class SignUpForm(UserCreationForm):
 		fields = (
 			'account_id',
 		)
+
 	def __init__(self, *args,**kwargs):
 		super().__init__(*args,**kwargs)
 		self.fields['password1'].help_text = "あなたの他の個人情報と似ているパスワードにはできません。\nパスワードは最低 8 文字以上必要です。\nよく使われるパスワードにはできません。\n数字だけのパスワードにはできません。"
-		self.fields['account_id'].help_text = "アカウント名"
+		self.fields['account_id'].help_text = "ログインにも使う"
+
+		self.fields['account_id'].label = "ユーザー名"
 
 class LoginForm(AuthenticationForm):
 	class Meta:
